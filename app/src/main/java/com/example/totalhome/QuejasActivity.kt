4f70c803e4e9a20cf49419b8c2a5ec43f4435a09
iamArianNieto <ari066@hotmail.com>
2025-06-24 00:03:07 -0600

    se anexa seccion de quejas

---
+++ app/src/main/java/com/example/totalhome/QuejasActivity.kt
@@ -0,0 +1,15 @@
+package com.example.totalhome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class QuejasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quejas)
+
+
+    }
+}
