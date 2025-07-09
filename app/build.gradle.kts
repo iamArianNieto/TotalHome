11f939b4165092b4bbeb056bf28784bef00a099b
iamArianNieto <ari066@hotmail.com>
2025-07-09 00:25:04 -0600

    se anexa pantalla nuevo invitado falta adaptar a material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -39,9 +41,20 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+        // Material Design 3
+        implementation("com.google.android.material:material:1.12.0")
+
+        // CircleImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+
+
+
+
 }
